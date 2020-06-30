706ca3cd3c412aa795b47135cbb406059f5fd1f8
Alexander Calvillo <alexanderjcalvillo@gmail.com>
2020-06-30 10:46:56 -0500

    110-node-numbers challenge complete

--- challenges/110-node-numbers.js
+++ challenges/110-node-numbers.js
@@ -1,10 +1,13 @@
 // run `node test.js` in the terminal, to see what happens.
 // We have failing messages! That's no good!
 // What is `test.js` trying to do?
+// the code in test.js is running, however none of the expressions checked
+// in the if statements are true, so it runs the else sections which
+// say they failed.
 
 // module.exports allows us to share information in one file with another file
 // change the number to 20, the number of mushers we have available
 
-module.exports = 0;
+module.exports = 20;
 
 // now run `node test.js` in the terminal, to see what happens
